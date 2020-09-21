e16abca7db777d680c9bc593ae1fc0e8ca49d5fc
IlonaStr <hellosidney17@gmail.com>
2020-09-21 18:06:51 +0300

    Updates

---
+++ precache-manifest.9e505c73ae5b9735f2589301525c9aae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c63af37380f966e3af135b4be4d4fd08",
+    "url": "/goit-react-hw-01-components/index.html"
+  },
+  {
+    "revision": "9c988ba9252c58abaca7",
+    "url": "/goit-react-hw-01-components/static/css/main.b5a43617.chunk.css"
+  },
+  {
+    "revision": "504789db751ccb393e7b",
+    "url": "/goit-react-hw-01-components/static/js/2.09e0f907.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-01-components/static/js/2.09e0f907.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c988ba9252c58abaca7",
+    "url": "/goit-react-hw-01-components/static/js/main.46c34a7a.chunk.js"
+  },
+  {
+    "revision": "7181ea73fa66db6b9cd4",
+    "url": "/goit-react-hw-01-components/static/js/runtime-main.46575509.js"
+  }
+]);
